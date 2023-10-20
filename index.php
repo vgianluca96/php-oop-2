74dcf44989f92756f1461c051d2ef7de140eda33
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-20 14:24:29 +0200

    initial commit
    preparato index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$message = 'ciao';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 2</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container py-4">
+
+        <?php
+        echo $message;
+        ?>
+    </div>
+
+</body>
+
+</html>
